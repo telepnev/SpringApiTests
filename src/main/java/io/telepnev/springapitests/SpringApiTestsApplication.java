92c15a9f69e277cae844ee0a8a71a905c4aeac55
Evgeniy Telepnev <telepnef@yandex.ru>
2023-03-09 13:46:50 +0300

    Initial commit

---
+++ src/main/java/io/telepnev/springapitests/SpringApiTestsApplication.java
@@ -0,0 +1,13 @@
+package io.telepnev.springapitests;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringApiTestsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringApiTestsApplication.class, args);
+    }
+
+}
---
+++ src/main/java/io/telepnev/springapitests/controller/BankController.java
@@ -0,0 +1,7 @@
+package io.telepnev.springapitests.controller;
+
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class BankController {
+}
